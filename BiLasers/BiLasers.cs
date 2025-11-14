12f578613d6bd844a2601e3988d81c948e58bae5
IHaveAName2653 <89300269+IHaveAName2653@users.noreply.github.com>
2025-11-14 14:50:10 +1100

    Colors update on change, Refactor
    Changed some variable names, and added some functions for getting a config value as if it was just a normal variable.
    Sorted the creation section into regions as well.
    Still doesnt call update though, that is next.

--- BiLasers/BiLasers.cs
+++ BiLasers/BiLasers.cs
@@ -19,62 +19,104 @@ namespace RainbowLasers
         public override string Link => "https://github.com/l79627550-dot/BiLasers";
         public override string Version => "1.1.0";
 
-        public static ModConfiguration config;
+        public static ModConfiguration? config;
 
         [AutoRegisterConfigKey]
-        private static readonly ModConfigurationKey<bool> ENABLED = new ModConfigurationKey<bool>("enabled", "Enabled", () => true);
+        private static readonly ModConfigurationKey<bool> ENABLED = new ModConfigurationKey<bool>("enabled", "Enabled", () => DefaultEnabled);
 
         [AutoRegisterConfigKey]
-        private static readonly ModConfigurationKey<float> SMOOTH = new ModConfigurationKey<float>("Smoothing Value", "How fast do you want the laser to change color?", () => 10f);
+        private static readonly ModConfigurationKey<float> SMOOTH = new ModConfigurationKey<float>("Smoothing Value", "How fast do you want the laser to change color?", () => DefaultSmoothSpeed);
 
         [AutoRegisterConfigKey]
-        private static readonly ModConfigurationKey<colorX> START = new ModConfigurationKey<colorX>("Start Color", "Start Color:", () => new colorX(0.84f, 0.01f, 0.44f, 1f));
+        private static readonly ModConfigurationKey<colorX> START = new ModConfigurationKey<colorX>("Start Color", "Start Color:", () => DefaultStartColor);
 
         [AutoRegisterConfigKey]
-        private static readonly ModConfigurationKey<colorX> END = new ModConfigurationKey<colorX>("End Color", "End Color", () => new colorX(0f, 0.22f, 0.66f, 1f));
+        private static readonly ModConfigurationKey<colorX> END = new ModConfigurationKey<colorX>("End Color", "End Color", () => DefaultEndColor);
 
-        struct LaserData
-        {
-            public InteractionLaser laser;
-            public IAssetProvider<Mesh> originalMesh;
-            public IAssetProvider<Mesh> newMesh;
-            public SmoothValue<colorX> startSmooth;
-            public SmoothValue<colorX> endSmooth;
-            public bool IsNew;
-        }
+        // Defaults
+        static readonly bool DefaultEnabled = true;
+        static readonly float DefaultSmoothSpeed = 10;
+        static readonly colorX DefaultStartColor = new colorX(0.84f, 0.01f, 0.44f, 1f);
+        static readonly colorX DefaultEndColor = new colorX(0f, 0.22f, 0.66f, 1f);
 
-        static List<LaserData> lasers;
+        // Dynamic Variable Names. {0}=L/R, {1}=Start/End
+        static readonly string BaseColorVariable = "User/InteractionLaser_{0}_{1}";
+        static readonly string NewColorVariable = "User/Laser_{0}_{1}";
 
         public override void OnEngineInit()
         {
             config = GetConfiguration();
 
-            config.OnThisConfigurationChanged += OnConfigChange;
-
-            config.Save(true);
-            Harmony harmony = new Harmony("com.zahndy.RainbowLasers");
+            if (config != null)
+            {
+                config.OnThisConfigurationChanged += OnConfigChange;
+                config.Save(true);
+            }
+            Harmony harmony = new Harmony("com.nexis.BiLasers");
             harmony.PatchAll();
         }
 
-        private void OnConfigChange(ConfigurationChangedEvent configurationChangedEvent)
+        // Quick way to get the config values, with proper nullchecks
+        #region Config Value Getters
+        static bool Enabled => GetConfigValue(ENABLED, DefaultEnabled);
+        static float SmoothSpeed => GetConfigValue(SMOOTH, DefaultSmoothSpeed);
+        static colorX StartColor => GetConfigValue(START, DefaultStartColor);
+        static colorX EndColor => GetConfigValue(END, DefaultEndColor);
+
+        static T GetDefault<T>(ModConfigurationKey<T> key, T defaultValue)
         {
-            bool isEnabled = config.GetValue(ENABLED);
-            float speed = config.GetValue(SMOOTH);
+            key.TryComputeDefault(out object? value);
+            if (value == null) return defaultValue;
+            return (T)value;
+        }
+
+        static T GetConfigValue<T>(ModConfigurationKey<T> key, T defaultValue)
+        {
+            if (config != null) return config.GetValue(key) ?? defaultValue;
+            return GetDefault(key, defaultValue);
+        }
+
+        #endregion
+
+        struct LaserData
+        {
+            public InteractionLaser laser;
+
+            public IAssetProvider<Mesh> originalMesh;
+            public IAssetProvider<Mesh> newMesh;
+
+            public SmoothValue<colorX> startSmooth;
+            public SmoothValue<colorX> endSmooth;
+
+            public Sync<colorX> newStartColor;
+            public Sync<colorX> newEndColor;
+        }
+
+        static List<LaserData> lasers = [];
+
+        static void OnConfigChange(ConfigurationChangedEvent configurationChangedEvent)
+        {
+
             for (int i = 0; i < lasers.Count; i++)
             {
                 LaserData thisLaser = lasers[i];
 
-                if (thisLaser.laser.IsDestroyed || thisLaser.laser == null)
-                {   
+                // If the component is gone, by nulling, disposing or destroying
+                if (thisLaser.laser == null || thisLaser.laser.IsRemoved)
+                {
                     lasers.RemoveAt(i);
                     i--;
+                    continue;
                 }
 
-                thisLaser.startSmooth.Speed.Value = speed;
-                thisLaser.endSmooth.Speed.Value = speed;
+                thisLaser.startSmooth.Speed.Value = SmoothSpeed;
+                thisLaser.endSmooth.Speed.Value = SmoothSpeed;
+
+                thisLaser.newStartColor.Value = StartColor;
+                thisLaser.newEndColor.Value = EndColor;
 
                 AssetRef<Mesh> Renderer = thisLaser.laser.Slot.GetComponent<MeshRenderer>().Mesh;
-                Renderer.Target = isEnabled ? thisLaser.newMesh : thisLaser.originalMesh;
+                Renderer.Target = Enabled ? thisLaser.newMesh : thisLaser.originalMesh;
             }
         }
 
@@ -83,7 +125,7 @@ namespace RainbowLasers
         {
             [HarmonyPrefix]
             [HarmonyPatch("OnAwake")]
-            static void Prefix(InteractionLaser __instance,
+            public static void Prefix(InteractionLaser __instance,
                                FieldDrive<colorX> ____startColor,
                                FieldDrive<colorX> ____endColor,
                                FieldDrive<float3> ____directPoint,
@@ -92,90 +134,116 @@ namespace RainbowLasers
             {
                 __instance.RunInUpdates(3, () =>
                 {
-                    // Always create variables, but dont assign if not enabled
-                    bool isEnabled = config.GetValue(ENABLED);
+                    // Always create variables, but dont change mesh if not enabled
                     if (__instance.Slot.ActiveUserRoot.ActiveUser != __instance.LocalUser) return;
 
-                    Slot Assets = __instance.Slot.AddSlot("Assets");
 
-                    LaserData thisLaser = new LaserData();
-                    thisLaser.laser = __instance;
-                    thisLaser.IsNew = isEnabled;
-
-                    ValueMultiDriver<colorX> startColors = Assets.AttachComponent<ValueMultiDriver<colorX>>();
-                    ValueMultiDriver<colorX> endColors = Assets.AttachComponent<ValueMultiDriver<colorX>>();
-
-                    ValueMultiDriver<float3> directTargetPoint = Assets.AttachComponent<ValueMultiDriver<float3>>();
-                    ValueMultiDriver<float3> actualTargetPoint = Assets.AttachComponent<ValueMultiDriver<float3>>();
-
-                    DynamicValueVariable<colorX> ColS = Assets.AttachComponent<DynamicValueVariable<colorX>>(); // Start
-                    DynamicValueVariable<colorX> ColE = Assets.AttachComponent<DynamicValueVariable<colorX>>(); // End
-
-                    DynamicValueVariableDriver<colorX> StartColor = Assets.AttachComponent<DynamicValueVariableDriver<colorX>>();
-                    DynamicValueVariableDriver<colorX> EndColor = Assets.AttachComponent<DynamicValueVariableDriver<colorX>>();
-
-                    SmoothValue<colorX> ColSS = Assets.AttachComponent<SmoothValue<colorX>>();
-                    SmoothValue<colorX> ColES = Assets.AttachComponent<SmoothValue<colorX>>();
-                    thisLaser.startSmooth = ColSS;
-                    thisLaser.endSmooth = ColES;
+                    Slot Assets = __instance.Slot.AddSlot("Laser Assets");
 
                     bool side = ____handler.Target.Side.Value == Chirality.Right;
-                    StartColor.VariableName.Value = $"User/Laser_{(side ? "R" : "L")}_Start";
-                    EndColor.VariableName.Value = $"User/Laser_{(side ? "R" : "L")}_End";
-                    ColS.VariableName.Value = $"User/InteractionLaser_{(side ? "R" : "L")}_Start";
-                    ColE.VariableName.Value = $"User/InteractionLaser_{(side ? "R" : "L")}_End";
+                    string sideAsLR = side ? "R" : "L";
 
-                    BentTubeMesh Mesh = Assets.AttachComponent<BentTubeMesh>();
-                    AssetRef<Mesh> Renderer = __instance.Slot.GetComponent<MeshRenderer>().Mesh;
+                    #region Components
+
+                    BentTubeMesh NewMesh = Assets.AttachComponent<BentTubeMesh>();
+                    NewMesh.Radius.Value = 0.002f;
+                    NewMesh.Sides.Value = 6;
+                    NewMesh.Segments.Value = 16;
+
                     BentTubeMesh OriginalMesh = __instance.Slot.GetComponent<BentTubeMesh>();
 
-                    thisLaser.originalMesh = Renderer.Target;
-                    thisLaser.newMesh = Mesh;
+                    AssetRef<Mesh> Renderer = __instance.Slot.GetComponent<MeshRenderer>().Mesh;
 
-                    StartColor.Target.Value = ColSS.TargetValue.ReferenceID;
-                    EndColor.Target.Value = ColES.TargetValue.ReferenceID;
+                    // Original colors
+                    ValueMultiDriver<colorX> baseStartColorDrives = Assets.AttachComponent<ValueMultiDriver<colorX>>();
+                    ValueMultiDriver<colorX> baseEndColorDrives = Assets.AttachComponent<ValueMultiDriver<colorX>>();
 
-                    ColSS.Value.Value = Mesh.StartPointColor.ReferenceID;
-                    ColES.Value.Value = Mesh.EndPointColor.ReferenceID;
+                    DynamicValueVariable<colorX> originalStartColor = Assets.AttachComponent<DynamicValueVariable<colorX>>(); // Start
+                    DynamicValueVariable<colorX> originalEndColor = Assets.AttachComponent<DynamicValueVariable<colorX>>(); // End
+                    originalStartColor.VariableName.Value = string.Format(BaseColorVariable, sideAsLR, "Start");
+                    originalEndColor.VariableName.Value = string.Format(BaseColorVariable, sideAsLR, "End");
 
-                    ColSS.Speed.Value = config.GetValue(SMOOTH);
-                    ColES.Speed.Value = config.GetValue(SMOOTH);
+                    // New colors
+                    DynamicValueVariableDriver<colorX> newStartColor = Assets.AttachComponent<DynamicValueVariableDriver<colorX>>();
+                    DynamicValueVariableDriver<colorX> newEndColor = Assets.AttachComponent<DynamicValueVariableDriver<colorX>>();
+                    newStartColor.VariableName.Value = string.Format(NewColorVariable, sideAsLR, "Start");
+                    newEndColor.VariableName.Value = string.Format(NewColorVariable, sideAsLR, "End");
+                    newStartColor.DefaultValue.Value = StartColor;
+                    newEndColor.DefaultValue.Value = EndColor;
 
-                    StartColor.DefaultValue.Value = config.GetValue(START);
-                    EndColor.DefaultValue.Value = config.GetValue(END);
+                    // Smooth values
+                    SmoothValue<colorX> newStartSmoothed = Assets.AttachComponent<SmoothValue<colorX>>();
+                    SmoothValue<colorX> newEndSmoothed = Assets.AttachComponent<SmoothValue<colorX>>();
+                    newStartSmoothed.Speed.Value = SmoothSpeed;
+                    newEndSmoothed.Speed.Value = SmoothSpeed;
 
-                    if (isEnabled) Renderer.Target = Mesh;
-                    Mesh.Radius.Value = 0.002f;
-                    Mesh.Sides.Value = 6;
-                    Mesh.Segments.Value = 16;
+                    // End points
+                    ValueMultiDriver<float3> directTargetPoints = Assets.AttachComponent<ValueMultiDriver<float3>>();
+                    ValueMultiDriver<float3> actualTargetPoints = Assets.AttachComponent<ValueMultiDriver<float3>>();
 
-                    startColors.Drives.Add();
-                    startColors.Drives.Add();
-                    startColors.Drives[0].ForceLink(OriginalMesh.StartPointColor);
-                    startColors.Drives[1].ForceLink(ColS.Value);
-                    endColors.Drives.Add();
-                    endColors.Drives.Add();
-                    endColors.Drives[0].ForceLink(OriginalMesh.EndPointColor);
-                    endColors.Drives[1].ForceLink(ColE.Value);
-                    
-                    ____startColor.Value = startColors.Value.ReferenceID;
-                    ____endColor.Value = endColors.Value.ReferenceID;
+                    #endregion
 
-                    directTargetPoint.Drives.Add();
-                    directTargetPoint.Drives.Add();
-                    directTargetPoint.Drives[0].ForceLink(OriginalMesh.DirectTargetPoint);
-                    directTargetPoint.Drives[1].ForceLink(Mesh.DirectTargetPoint);
-                    actualTargetPoint.Drives.Add();
-                    actualTargetPoint.Drives.Add();
-                    actualTargetPoint.Drives[0].ForceLink(OriginalMesh.ActualTargetPoint);
-                    actualTargetPoint.Drives[1].ForceLink(Mesh.ActualTargetPoint);
+                    #region Drives
 
-                    ____directPoint.ForceLink(directTargetPoint.Value);
-                    ____actualPoint.ForceLink(actualTargetPoint.Value);
+                    newStartColor.Target.Value = newStartSmoothed.TargetValue.ReferenceID;
+                    newEndColor.Target.Value = newEndSmoothed.TargetValue.ReferenceID;
 
-                    __instance.Enabled = true;
+                    newStartSmoothed.Value.Value = NewMesh.StartPointColor.ReferenceID;
+                    newEndSmoothed.Value.Value = NewMesh.EndPointColor.ReferenceID;
+
+                    ____startColor.Value = baseStartColorDrives.Value.ReferenceID;
+                    ____endColor.Value = baseEndColorDrives.Value.ReferenceID;
+
+                    ____directPoint.ForceLink(directTargetPoints.Value);
+                    ____actualPoint.ForceLink(actualTargetPoints.Value);
+
+                    // Create all drives required
+                    const int DRIVE_COUNT = 2;
+                    for (int i = 0; i < DRIVE_COUNT; i++)
+                    {
+                        baseStartColorDrives.Drives.Add();
+                        baseEndColorDrives.Drives.Add();
+                        directTargetPoints.Drives.Add();
+                        actualTargetPoints.Drives.Add();
+                    }
+
+                    baseStartColorDrives.Drives[0].ForceLink(OriginalMesh.StartPointColor);
+                    baseStartColorDrives.Drives[1].ForceLink(originalStartColor.Value);
+
+                    baseEndColorDrives.Drives[0].ForceLink(OriginalMesh.EndPointColor);
+                    baseEndColorDrives.Drives[1].ForceLink(originalEndColor.Value);
+
+                    directTargetPoints.Drives[0].ForceLink(OriginalMesh.DirectTargetPoint);
+                    directTargetPoints.Drives[1].ForceLink(NewMesh.DirectTargetPoint);
+
+                    actualTargetPoints.Drives[0].ForceLink(OriginalMesh.ActualTargetPoint);
+                    actualTargetPoints.Drives[1].ForceLink(NewMesh.ActualTargetPoint);
+
+                    #endregion
+
+                    #region Laser data struct
+
+                    LaserData thisLaser = new()
+                    {
+                        laser = __instance,
+
+                        originalMesh = OriginalMesh,
+                        newMesh = NewMesh,
+
+                        startSmooth = newStartSmoothed,
+                        endSmooth = newEndSmoothed,
+
+                        newStartColor = newStartColor.DefaultValue,
+                        newEndColor = newEndColor.DefaultValue
+                    };
 
                     lasers.Add(thisLaser);
+
+                    #endregion
+
+                    if (Enabled) Renderer.Target = NewMesh;
+
+                    __instance.Enabled = true;
                 });
             }
         }
